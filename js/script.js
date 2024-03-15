f352b8e37d820d56384e0f7bd5a64e0fe2bfdf03
MiguelMoreiraDev <contatomigueldev@gmail.com>
2024-03-15 17:24:54 -0300

    validation form

--- js/script.js
+++ js/script.js
@@ -4,28 +4,32 @@ let altura = document.getElementById("altura");
 const button = document.querySelector(".button");
 
 button.addEventListener("click", (event) => {
-    event.preventDefault();
-    let valorPeso = peso.value;
-    let valorAltura = altura.value;
+	if (peso.value && altura.value) {
+		event.preventDefault();
+		let valorPeso = peso.value;
+		let valorAltura = altura.value;
 
-    let imc = (valorPeso / valorAltura ** 2).toFixed(2);
+		let imc = (valorPeso / valorAltura ** 2).toFixed(2);
 
-    let desc = document.getElementById("desc");
-    let value = document.getElementById("value");
-    value.innerText = imc;
+		let desc = document.getElementById("desc");
+		let value = document.getElementById("value");
+		value.innerText = imc;
 
-    if (imc < 18.5) {
-        desc.innerText = "Você está magro";
-    } else if (imc > 18.5 && imc < 24.9) {
-        desc.innerText = "Você está em seu peso ideal!";
-    } else if (imc > 25 && imc < 29.9) {
-        desc.innerText = "Você está com sobrepeso";
-    } else if (imc > 30 && imc < 39.9) {
-        desc.innerText = "Você está com obesidade";
-    } else if (imc > 40) {
-        desc.innerText = "Você está com obesidade grave";
-    }
+		if (imc < 18.5) {
+			desc.innerText = "Você está magro";
+		} else if (imc > 18.5 && imc < 24.9) {
+			desc.innerText = "Você está em seu peso ideal!";
+		} else if (imc > 25 && imc < 29.9) {
+			desc.innerText = "Você está com sobrepeso";
+		} else if (imc > 30 && imc < 39.9) {
+			desc.innerText = "Você está com obesidade";
+		} else if (imc > 40) {
+			desc.innerText = "Você está com obesidade grave";
+		}
 
-    const result = document.querySelector(".calculator-mid");
-    result.classList.remove("hidden");
+		const result = document.querySelector(".calculator-mid");
+		result.classList.remove("hidden");
+	} else {
+		alert("Preecnha os campos");
+	}
 });

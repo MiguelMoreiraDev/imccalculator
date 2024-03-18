a36e9b2c5a7066db24fb8f84dd1d6e3aa8bab966
MiguelMoreiraDev <contatomigueldev@gmail.com>
2024-03-18 10:38:56 -0300

    animations

--- js/script.js
+++ js/script.js
@@ -1,35 +1,39 @@
-let peso = document.getElementById("peso");
-let altura = document.getElementById("altura");
+function initCalcImc() {
+	let peso = document.getElementById("peso");
+	let altura = document.getElementById("altura");
 
-const button = document.querySelector(".button");
+	const button = document.querySelector(".button");
 
-button.addEventListener("click", (event) => {
-	if (peso.value && altura.value) {
-		event.preventDefault();
-		let valorPeso = peso.value;
-		let valorAltura = altura.value;
+	button.addEventListener("click", (event) => {
+		if (peso.value && altura.value) {
+			event.preventDefault();
+			let valorPeso = peso.value;
+			let valorAltura = altura.value;
 
-		let imc = (valorPeso / valorAltura ** 2).toFixed(2);
+			let imc = (valorPeso / valorAltura ** 2).toFixed(2);
 
-		let desc = document.getElementById("desc");
-		let value = document.getElementById("value");
-		value.innerText = imc;
+			let desc = document.getElementById("desc");
+			let value = document.getElementById("value");
+			value.innerText = imc;
 
-		if (imc < 18.5) {
-			desc.innerText = "Você está magro";
-		} else if (imc > 18.5 && imc < 24.9) {
-			desc.innerText = "Você está em seu peso ideal!";
-		} else if (imc > 25 && imc < 29.9) {
-			desc.innerText = "Você está com sobrepeso";
-		} else if (imc > 30 && imc < 39.9) {
-			desc.innerText = "Você está com obesidade";
-		} else if (imc > 40) {
-			desc.innerText = "Você está com obesidade grave";
+			if (imc < 18.5) {
+				desc.innerText = "Você está magro";
+			} else if (imc > 18.5 && imc < 24.9) {
+				desc.innerText = "Você está em seu peso ideal!";
+			} else if (imc > 25 && imc < 29.9) {
+				desc.innerText = "Você está com sobrepeso";
+			} else if (imc > 30 && imc < 39.9) {
+				desc.innerText = "Você está com obesidade";
+			} else if (imc > 40) {
+				desc.innerText = "Você está com obesidade grave";
+			}
+
+			const result = document.querySelector(".calculator-mid");
+			result.classList.remove("hidden");
+		} else {
+			alert("Preencha os campos");
 		}
+	});
+}
 
-		const result = document.querySelector(".calculator-mid");
-		result.classList.remove("hidden");
-	} else {
-		alert("Preecnha os campos");
-	}
-});
+initCalcImc();
